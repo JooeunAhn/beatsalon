7083ef8043b18cd4c78197ce985e70b8904a5c87
rheehyerin <toohappy2122@gmail.com>
2016-08-06 17:43:55 +0900

    resize

--- salon/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalonConfig(AppConfig):
-    name = 'salon'
--- salon/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Audio(models.Model):
-    videoId = models.CharField(max_length=100, unique=True)
