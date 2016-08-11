dfa1eb3696db2d2f87c558ba0ee167a49ef208b6
JooeunAhn <jflow@hanyang.ac.kr>
2016-08-11 19:20:46 +0900

    댓글 ajax

--- beatsalon/salon/views.py
+++ beatsalon/salon/views.py
@@ -5,18 +5,19 @@ from .forms import CommentForm
 from .models import Comment
 
 def index(request):
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_ajax():
         form = CommentForm(request.POST, request.FILES)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.save()
             comments = Comment.objects.order_by('pk').reverse()
-            return render(request, 'salon/index.html', {'comments': comments})
+            return render(request, 'salon/comment.html', {'comments': comments})
     else:
         form = CommentForm()
     comments = Comment.objects.order_by('pk').reverse()
     return render(request, 'salon/index.html',
-        {'comments': comments}
+        {'comments': comments,
+         'form':form,}
     )
 
 
@@ -26,15 +27,16 @@ def my_hook(status):
         print("Done downloading, now converting ...")
 
 def serve(request,pk):
-    if request.method == 'POST':
+    if request.method == 'POST' and request.is_ajax():
         form = CommentForm(request.POST, request.FILES)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.save()
             comments = Comment.objects.order_by('pk').reverse()
-            return render(request, 'salon/index.html', {'comments': comments})
+            return render(request, 'salon/comment.html', {'comments': comments})
     else:
         form = CommentForm()
+
     comments = Comment.objects.order_by('pk').reverse()
     ydl_opt = {
             'format':'bestaudio/best',
@@ -56,4 +58,4 @@ def serve(request,pk):
             info = ydl.extract_info("https://youtu.be/"+pk, download=False)
     thumbnail_url = info['thumbnails'][0]["url"]
     return render(request, 'salon/serve.html', {'info':info, "thumbnail_url":thumbnail_url, 'comments': comments,
-        'musicId':pk,})
+        'musicId':pk,'form':form})
